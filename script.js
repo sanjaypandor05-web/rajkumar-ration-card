c7793d203115cc3015dcb9fa853c2ec847c77c1d
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-07-28 14:55:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,7 @@
 
 
 const SCRIPT_URL =
-"https://script.google.com/macros/s/AKfycbz4dmeD7H-AMwhJYRn0CHGlkGnbXCy4bEstvR0INigrAzuqnVNjKQ5cL1VBFHqN4jJwTg/exec";
-
+"https://script.google.com/macros/s/AKfycbyFrg_3lB5WKelkE2vsRW4ZaK7PyvM5F93A-Jfzqla3y8gSQIKei_QZBet9VwewQIXg/exec";
 
 
 
