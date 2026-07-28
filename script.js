b3076f795873e793e2bc1356cebb181aed287529
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-07-28 09:41:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // RAJKUMAR RATION CARD ONLINE WORK V5
 // =====================================
 
-const SCRIPT_URL = "hhttps://script.google.com/macros/s/AKfycbzvhEzTOFCwMq1wrOJpOzEm07wQoKucAshnPAx2jsl0ETHc4CfQTU4WqlkbHfZnsa0_RA/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzvhEzTOFCwMq1wrOJpOzEm07wQoKucAshnPAx2jsl0ETHc4CfQTU4WqlkbHfZnsa0_RA/exec";
 
 
 document.addEventListener("DOMContentLoaded", () => {
