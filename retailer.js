b76d778148299b3bb8582efe56347dc6063a8f95
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-07-31 17:57:43 +0530

    Update retailer.js

--- retailer.js
+++ retailer.js
@@ -6,7 +6,7 @@
 // ---------- GOOGLE APPS SCRIPT URL ----------
 
 const SCRIPT_URL =
-"https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzjuIA4WgEfDYqev1deYRoDoMG4LPbRXA7MUxCrxCeV16Tncsy8phTCNoNckRUpXs-umA/exec";
 
 // ---------- GLOBAL VARIABLES ----------
 
