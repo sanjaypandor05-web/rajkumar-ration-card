2847063cabb4e5675ccab9ea9513cfa29a16e1bc
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-08-04 20:36:46 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@
 // LOGIN.JS - PART 1
 // ==========================================
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxY-3pyd__jXe76CaK2IRf6aWgfVaim3TxeM0bnIF57P4yhPO4cEeSP8RqxK2IjO8PGRA/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyP3L_6EWo3AUxweac-rjG_cpwl-GpNAozuCbmjNOQSLSJwfOETYcxeB2YHlFSJ-zeAwQ/exec";
 
 // ================= SHOW / HIDE PASSWORD =================
 
