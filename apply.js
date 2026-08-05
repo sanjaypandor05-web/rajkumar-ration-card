ac1a7ef8b7bb2e4275dc033842be6db8108b16a6
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-08-05 17:26:02 +0530

    Add files via upload

---
+++ apply.js
@@ -0,0 +1,134 @@
+
+// ===================================
+// RAJKUMAR RATION CARD
+// APPLY JS
+// ===================================
+
+
+// Google Apps Script Web App URL
+const SCRIPT_URL = "YOUR_APPS_SCRIPT_URL";
+
+
+
+
+
+function sendApplication(){
+
+
+
+let name = document.getElementById("name").value;
+
+let mobile = document.getElementById("mobile").value;
+
+let village = document.getElementById("village").value;
+
+let service = document.getElementById("service").value;
+
+
+
+let msg = document.getElementById("msg");
+
+
+
+
+
+if(name=="" || mobile=="" || village=="" || service==""){
+
+
+msg.innerHTML =
+"❌ બધી માહિતી ફરજિયાત ભરો";
+
+
+msg.style.color="red";
+
+
+return;
+
+
+}
+
+
+
+
+
+let data = {
+
+
+name:name,
+
+mobile:mobile,
+
+village:village,
+
+service:service
+
+
+};
+
+
+
+
+
+fetch(SCRIPT_URL,{
+
+
+method:"POST",
+
+body:JSON.stringify(data)
+
+
+})
+
+
+
+.then(response=>response.json())
+
+
+
+.then(result=>{
+
+
+if(result.status=="success"){
+
+
+msg.innerHTML =
+"✅ અરજી સફળતાપૂર્વક મોકલાઈ<br>તમારી અરજી ID : "
++ result.id;
+
+
+msg.style.color="green";
+
+
+
+document.getElementById("name").value="";
+
+document.getElementById("mobile").value="";
+
+document.getElementById("village").value="";
+
+document.getElementById("service").value="";
+
+
+}
+
+
+
+})
+
+
+
+.catch(error=>{
+
+
+msg.innerHTML =
+"❌ Server Error";
+
+
+msg.style.color="red";
+
+
+});
+
+
+
+}
