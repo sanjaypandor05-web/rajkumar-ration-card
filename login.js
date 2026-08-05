d561e978d4711a30e09922b99e1dfcc29fb341d8
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-08-05 22:48:53 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@
 
 
 const SCRIPT_URL =
-"https://script.google.com/macros/s/AKfycbweg8Yrtw-CpQqa5h5W6pk0WofK6tnPcKd5SWFf9BPkpxDxrv87WR5wsRF5N6E2lj4C2g/exec";
+"https://script.google.com/macros/s/AKfycby5iGcA2gn3X6l37EkJvjt80XgpNnutj4fT6tCLG46Om1W_3_huu5hDONOwMFBUn8tqKQ/exec";
 
 
 
