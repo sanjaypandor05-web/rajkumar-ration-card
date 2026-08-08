ca6c0c2c0322d842799218ddff220a1c5c9dec35
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-08-08 21:04:03 +0530

    Update login.js

--- login.js
+++ login.js
@@ -9,7 +9,7 @@
 // =====================================
 
 const SCRIPT_URL =
-"https://script.google.com/macros/s/AKfycby1cAsNRzfvrSLqZrukmEjqIKbCCkOHaShKzmuvVvyVlipSRmaRw8KTefkdKSQgsJkPJg/exec";
+"https://script.google.com/macros/s/AKfycbwF4N8e9pxd3TYS7fdC73vDdCwZ2vQra_ckTrDYLRAaDvCeGvLeebnVCTpwTPTKMdkYyA/exec";
 
 
 // =====================================
